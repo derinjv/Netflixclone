3c25fdc634956f44de79447d1fbc7e7765734701
derinjv <derinjv@gmail.com>
2025-03-13 23:18:50 +0530

    updates

--- netflix/src/pages/Home/Home.jsx
+++ netflix/src/pages/Home/Home.jsx
@@ -6,31 +6,48 @@ import hero_title from "../../assets/hero_title.png";
 import play from "../../assets/play_icon.png";
 import info from "../../assets/info_icon.png";
 import Titlecards from "../../components/TitleCards/Titlecards";
+
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <div className="hero">
-        <img src={hero_banner} alt="" className="banner-img" />
+        <img
+          src={hero_banner}
+          alt="Hero Banner - Ancient Quest Adventure"
+          className="banner-img"
+        />
         <div className="hero-caption">
-          <img src={hero_title} alt="" className="caption-img" />
+          <img
+            src={hero_title}
+            alt="Hero Title - The Protector"
+            className="caption-img"
+          />
           <p>
             Discovering his ties to a secret ancient order, a young man living
             in modern Istanbul embarks on a quest to save the city from an
             immortal enemy.
           </p>
           <div className="hero-btns">
-            <button className="btn">
-              <img src={play} alt="" />
+            <button className="btn" aria-label="Play Trailer">
+              <img src={play} alt="Play Icon" />
               Play
             </button>
-            <button className="btn dark-btn">
-              <img src={info} alt="" />
+            <button className="btn dark-btn" aria-label="More Information">
+              <img src={info} alt="Info Icon" />
               More Info
             </button>
           </div>
-          <Titlecards />
         </div>
+      </div>
+      <section className="content-section">
+        <Titlecards title="Popular on Netflix" />
+      </section>
+      <div className="more-cards">
+        <Titlecards title="Blockbuster Movies" />
+        <Titlecards title="Only on Netflix" />
+        <Titlecards title="Upcoming" />
+        <Titlecards title="Top Picks" />
       </div>
     </div>
   );
